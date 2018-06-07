d661b95fbc744e353d5295cae1696ee10ff22054
Arjun K R <arjunarjunkr@hotmail.com>
2018-06-07 12:02:23 +0530

    Initial commit

---
+++ KML2JSON/kml2json/Helper.cs
@@ -0,0 +1,104 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace kml2json
+{
+    class Helper
+    {
+        public void GetCordinates(string fileUri, string folder)
+        {
+            XmlReader reader = XmlReader.Create(@""+fileUri);
+            string filename = "default.txt";
+            string compiled = "";
+            while (reader.Read())
+            {
+                if ((reader.NodeType == XmlNodeType.Element) && reader.Name.Equals("name"))
+                {
+                    filename = reader.ReadString();
+                }
+                if ((reader.NodeType == XmlNodeType.Element) && reader.Name.Equals("coordinates"))
+                {
+                    compiled += ParseCordinates(reader.ReadString(),folder, filename);
+                }
+                 
+            }
+
+            using (StreamWriter writetext = new StreamWriter(folder + "\\json\\" + filename + ".txt"))
+            {
+                writetext.Write(compiled);
+            }
+
+        }
+
+        public void GetCordinatesMultiGeometry(string fileUri, string folder)
+        {
+           
+
+            XmlReader reader = XmlReader.Create(@"" + fileUri);
+            string filename = "";
+            int i = 1;
+            while (reader.Read())
+            {
+                if ((reader.NodeType == XmlNodeType.Element) && reader.Name.Equals("name"))
+                {
+                    filename = reader.ReadString();
+                }
+                if ((reader.NodeType == XmlNodeType.Element) && reader.Name.Equals("coordinates"))
+                {
+                    ParseCordinates(reader.ReadString(), folder, filename + " " + i);
+                    i++;
+                }             
+
+            }
+        }
+
+        public string ParseCordinates(string coOrdinates, string folder, string filename)
+        {
+            string compiled = "";
+            string[] str = coOrdinates.Trim().Split(new string[] { ",0" }, StringSplitOptions.None); //separated by ,0
+            if(str.Length<2)
+            {
+                str = coOrdinates.Trim().Split(new string[] { " " }, StringSplitOptions.None);//separated by ,<space>
+            }
+            if (str.Length < 2)
+            {
+                str = coOrdinates.Trim().Split(new string[] { "," }, StringSplitOptions.None);//separated by ,
+            }
+
+            int i = 0;
+            Directory.CreateDirectory(folder + "\\json");
+            JArray jsonArray = new JArray();
+            foreach (string s in str)
+            {
+                if (!s.Equals(""))
+                {
+                    
+                    string longitude = s.Trim().Split(new string[] { "," }, StringSplitOptions.None)[0];
+                    string latitude = s.Trim().Split(new string[] { "," }, StringSplitOptions.None)[1];
+
+                    JObject jsonObject = new JObject();
+                    jsonObject.Add("lat", latitude.Trim());
+                    jsonObject.Add("lng", longitude.Trim());
+
+                    jsonArray.Add(jsonObject);
+
+                    compiled = "\n"+ filename +","+ latitude.Trim() + "," + longitude.Trim();
+                }
+                i++;
+            }
+            Console.WriteLine("i: " + i);
+            using (StreamWriter writetext = new StreamWriter(folder+"\\json\\"+filename+".json"))
+            {
+                writetext.Write(jsonArray);
+            }
+
+            return compiled;
+
+        }
+
+    }
+}
---
+++ KML2JSON/kml2json/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using WinForms = System.Windows.Forms;
+
+namespace kml2json
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool multiGeometry = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void FileAddressButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            WinForms.FolderBrowserDialog fbd = new WinForms.FolderBrowserDialog();
+            WinForms.DialogResult result = fbd.ShowDialog();
+            fileAddressBox.IsReadOnly = false;
+            if (fbd.SelectedPath != null)
+            {
+                fileAddressBox.Text = fbd.SelectedPath;
+            }
+            fileAddressBox.IsReadOnly = true;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            startButton.IsEnabled = false;
+            Helper helper = new Helper();
+            fileAddressBox.IsReadOnly = false;
+            if ((fileAddressBox.Text != null)&&(fileAddressBox.Text.Length > 1))
+            {
+                if (Directory.Exists(fileAddressBox.Text))
+                {
+                    List<string> files = new List<string>(Directory.GetFiles(fileAddressBox.Text));
+                    int i = 0;
+                    files.Sort();
+                    foreach (string file in files)
+                    {
+                        if (file.EndsWith(".kml") || file.EndsWith(".KML"))
+                        {
+                            if (!multiGeometry)
+                                helper.GetCordinates(file, fileAddressBox.Text);
+                            else
+                                helper.GetCordinatesMultiGeometry(file, fileAddressBox.Text);
+                            i++;
+                        }
+                    }
+                    if (i > 0)
+                    {
+                        MessageBox.Show("Success!\n" + i + " files processed and saved at\n" + fileAddressBox.Text + "\\json");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No compatible file found! Make sure you have kml files in the selected directory.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Directory");
+                }
+            }
+            fileAddressBox.IsReadOnly = true;
+            startButton.IsEnabled = true;
+
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            fileAddressBox.Text = "Choose Directory...";
+        }
+
+        private void UtilizeImage_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://utilizesoftwares.com");
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            bool status = MultiGeometry.IsChecked.GetValueOrDefault();
+            if (status)
+            {
+                multiGeometry = true;
+            }
+            else
+            {
+                multiGeometry = false;
+            }
+        }
+
+        private void Host_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void InsertIntoDbButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
